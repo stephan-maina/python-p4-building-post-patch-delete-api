201a0a0c09449ce38c2043d363c5195a78771a89
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-24 01:04:28 +0300

    completed..

--- server/app.py
+++ server/app.py
@@ -1,86 +1,43 @@
-#!/usr/bin/env python3
-
-from flask import Flask, request, make_response
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-from models import db, User, Review, Game
+from flask import Flask, jsonify, request
+from models import db, Book
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/')
-def index():
-    return "Index for Game/Review/User API"
+@app.route('/books', methods=['GET'])
+def get_books():
+    books = Book.query.all()
+    book_list = [book.to_dict() for book in books]
+    return jsonify(book_list)
 
-@app.route('/games')
-def games():
+@app.route('/books', methods=['POST'])
+def create_book():
+    data = request.json
+    new_book = Book(title=data['title'], author=data['author'])
+    db.session.add(new_book)
+    db.session.commit()
+    return jsonify(new_book.to_dict()), 201
 
-    games = []
-    for game in Game.query.all():
-        game_dict = {
-            "title": game.title,
-            "genre": game.genre,
-            "platform": game.platform,
-            "price": game.price,
-        }
-        games.append(game_dict)
+@app.route('/books/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
+def manage_book(id):
+    book = Book.query.get_or_404(id)
 
-    response = make_response(
-        games,
-        200
-    )
+    if request.method == 'GET':
+        return jsonify(book.to_dict())
 
-    return response
+    if request.method == 'PATCH':
+        data = request.json
+        book.title = data.get('title', book.title)
+        book.author = data.get('author', book.author)
+        db.session.commit()
+        return jsonify(book.to_dict())
 
-@app.route('/games/<int:id>')
-def game_by_id(id):
-    game = Game.query.filter(Game.id == id).first()
-    
-    game_dict = game.to_dict()
-
-    response = make_response(
-        game_dict,
-        200
-    )
-
-    return response
-
-@app.route('/reviews')
-def reviews():
-
-    reviews = []
-    for review in Review.query.all():
-        review_dict = review.to_dict()
-        reviews.append(review_dict)
-
-    response = make_response(
-        reviews,
-        200
-    )
-
-    return response
-
-@app.route('/users')
-def users():
-
-    users = []
-    for user in User.query.all():
-        user_dict = user.to_dict()
-        users.append(user_dict)
-
-    response = make_response(
-        users,
-        200
-    )
-
-    return response
+    if request.method == 'DELETE':
+        db.session.delete(book)
+        db.session.commit()
+        return '', 204
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
